d047a6a36ec367bd53d61d4a3e0db5ad723daf24
Amit Mohite <mohite.amit@gmail.com>
2025-04-04 02:27:51 +0000

    udpated schedule to daily

--- dags/lead_scoring_data_pipeline/lead_scoring_data_pipeline.py
+++ dags/lead_scoring_data_pipeline/lead_scoring_data_pipeline.py
@@ -19,73 +19,73 @@ from lead_scoring_data_pipeline.schema import RAW_DATA_SCHEMA, MODEL_INPUT_SCHEM
 
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2023,9,14),
-    'retries' : 1, 
-    'retry_delay' : timedelta(seconds=5)
+    'start_date': datetime(2023, 9, 14),
+    'retries': 1,
+    'retry_delay': timedelta(seconds=5)
 }
 
 
 ML_data_cleaning_dag = DAG(
-                dag_id = 'Lead_Scoring_Data_Engineering_Pipeline',
-                default_args = default_args,
-                description = 'DAG to run data pipeline for lead scoring',
-                schedule_interval = '@daily',
-                catchup = False
+    dag_id='Lead_Scoring_Data_Engineering_Pipeline',
+    default_args=default_args,
+    description='DAG to run data pipeline for lead scoring',
+    schedule_interval='@daily',
+    catchup=False
 )
 
 ###############################################################################
 # Create a task for build_dbs() function with task_id 'building_db'
 # ##############################################################################
 building_db = PythonOperator(
-            task_id = 'building_db',
-            python_callable = build_dbs,
-            dag = ML_data_cleaning_dag)
+    task_id='building_db',
+    python_callable=build_dbs,
+    dag=ML_data_cleaning_dag)
 ###############################################################################
 # Create a task for raw_data_schema_check() function with task_id 'checking_raw_data_schema'
 # ##############################################################################
 checking_raw_data_schema = PythonOperator(
-        task_id = 'checking_raw_data_schema',
-        python_callable = raw_data_schema_check,
-        op_args=[CSV_DATA, RAW_DATA_SCHEMA],
-        dag = ML_data_cleaning_dag)
+    task_id='checking_raw_data_schema',
+    python_callable=raw_data_schema_check,
+    op_args=[CSV_DATA, RAW_DATA_SCHEMA],
+    dag=ML_data_cleaning_dag)
 ###############################################################################
 # Create a task for load_data_into_db() function with task_id 'loading_data'
 # #############################################################################
 loading_data = PythonOperator(
-        task_id = 'loading_data',
-        python_callable = load_data_into_db,
-        op_args=[CSV_DATA],
-        dag = ML_data_cleaning_dag)
+    task_id='loading_data',
+    python_callable=load_data_into_db,
+    op_args=[CSV_DATA],
+    dag=ML_data_cleaning_dag)
 ###############################################################################
 # Create a task for map_city_tier() function with task_id 'mapping_city_tier'
 # ##############################################################################
 mapping_city_tier = PythonOperator(
-        task_id = 'mapping_city_tier',
-        python_callable = map_city_tier,
-        dag = ML_data_cleaning_dag)
+    task_id='mapping_city_tier',
+    python_callable=map_city_tier,
+    dag=ML_data_cleaning_dag)
 ###############################################################################
 # Create a task for map_categorical_vars() function with task_id 'mapping_categorical_vars'
 # ##############################################################################
 mapping_categorical_vars = PythonOperator(
-        task_id = 'mapping_categorical_vars',
-        python_callable = map_categorical_vars,
-        dag = ML_data_cleaning_dag)
+    task_id='mapping_categorical_vars',
+    python_callable=map_categorical_vars,
+    dag=ML_data_cleaning_dag)
 ###############################################################################
 # Create a task for interactions_mapping() function with task_id 'mapping_interactions'
 # ##############################################################################
 mapping_interactions = PythonOperator(
-        task_id = 'mapping_interactions',
-        python_callable = interactions_mapping,
-        op_args=[TABLE_NAME],
-        dag = ML_data_cleaning_dag)
+    task_id='mapping_interactions',
+    python_callable=interactions_mapping,
+    op_args=[TABLE_NAME],
+    dag=ML_data_cleaning_dag)
 ###############################################################################
 # Create a task for model_input_schema_check() function with task_id 'checking_model_inputs_schema'
 # ##############################################################################
 checking_model_inputs_schema = PythonOperator(
-        task_id = 'checking_model_inputs_schema',
-        python_callable = model_input_schema_check,
-        op_args=[TABLE_NAME, MODEL_INPUT_SCHEMA],
-        dag = ML_data_cleaning_dag)
+    task_id='checking_model_inputs_schema',
+    python_callable=model_input_schema_check,
+    op_args=[TABLE_NAME, MODEL_INPUT_SCHEMA],
+    dag=ML_data_cleaning_dag)
 ###############################################################################
 # Define the relation between the tasks
 # ##############################################################################
--- dags/lead_scoring_inference_pipeline/lead_scoring_inference_pipeline.py
+++ dags/lead_scoring_inference_pipeline/lead_scoring_inference_pipeline.py
@@ -19,84 +19,84 @@ from lead_scoring_inference_pipeline.constants import CSV_DATA, TABLE_NAME
 
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2023,9,14),
-    'retries' : 1, 
-    'retry_delay' : timedelta(seconds=5)
+    'start_date': datetime(2023, 9, 14),
+    'retries': 1,
+    'retry_delay': timedelta(seconds=5)
 }
 
 
 Lead_scoring_inference_dag = DAG(
-                dag_id = 'Lead_scoring_inference_pipeline',
-                default_args = default_args,
-                description = 'Inference pipeline of Lead Scoring system',
-                schedule_interval = '@hourly',
-                catchup = False
+    dag_id='Lead_scoring_inference_pipeline',
+    default_args=default_args,
+    description='Inference pipeline of Lead Scoring system',
+    schedule_interval='@daily',
+    catchup=False
 )
 
 ##############################################################################
 # Task: Start MLflow Server (if not already running)
 ##############################################################################
 start_mlflow = PythonOperator(
-        task_id='start_mlflow_server',
-        python_callable=start_mlflow_server_if_not_running,
-        dag=Lead_scoring_inference_dag
+    task_id='start_mlflow_server',
+    python_callable=start_mlflow_server_if_not_running,
+    dag=Lead_scoring_inference_dag
 )
 
 ###############################################################################
 # Create a task for raw_data_schema_check() function with task_id 'checking_raw_data_schema'
 # ##############################################################################
 checking_raw_data_schema = PythonOperator(
-        task_id = 'checking_raw_data_schema',
-        python_callable = raw_data_schema_check,
-        op_args=[CSV_DATA, RAW_DATA_SCHEMA],
-        dag = Lead_scoring_inference_dag)
+    task_id='checking_raw_data_schema',
+    python_callable=raw_data_schema_check,
+    op_args=[CSV_DATA, RAW_DATA_SCHEMA],
+    dag=Lead_scoring_inference_dag)
 ###############################################################################
 # Create a task for load_data_into_db() function with task_id 'loading_data'
 # #############################################################################
 loading_data = PythonOperator(
-            task_id = 'loading_data',
-            python_callable = load_data_into_db,
-            op_args=[CSV_DATA],
-            dag = Lead_scoring_inference_dag)
+    task_id='loading_data',
+    python_callable=load_data_into_db,
+    op_args=[CSV_DATA],
+    dag=Lead_scoring_inference_dag)
 ###############################################################################
 # Create a task for map_city_tier() function with task_id 'mapping_city_tier'
 # ##############################################################################
 mapping_city_tier = PythonOperator(
-            task_id = 'mapping_city_tier',
-            python_callable = map_city_tier,
-            dag = Lead_scoring_inference_dag)
+    task_id='mapping_city_tier',
+    python_callable=map_city_tier,
+    dag=Lead_scoring_inference_dag)
 ###############################################################################
 # Create a task for map_categorical_vars() function with task_id 'mapping_categorical_vars'
 # ##############################################################################
 mapping_categorical_vars = PythonOperator(
-        task_id = 'mapping_categorical_vars',
-        python_callable = map_categorical_vars,
-        dag = Lead_scoring_inference_dag)
+    task_id='mapping_categorical_vars',
+    python_callable=map_categorical_vars,
+    dag=Lead_scoring_inference_dag)
 ###############################################################################
 # Create a task for interactions_mapping() function with task_id 'mapping_interactions'
 # ##############################################################################
 mapping_interactions = PythonOperator(
-        task_id = 'mapping_interactions',
-        python_callable = interactions_mapping,
-        op_args=[TABLE_NAME],
-        dag = Lead_scoring_inference_dag)
+    task_id='mapping_interactions',
+    python_callable=interactions_mapping,
+    op_args=[TABLE_NAME],
+    dag=Lead_scoring_inference_dag)
 ###############################################################################
 # Create a task for model_input_schema_check() function with task_id 'checking_model_inputs_schema'
 # ##############################################################################
 checking_model_inputs_schema = PythonOperator(
-            task_id = 'checking_model_inputs_schema',
-            python_callable = model_input_schema_check,
-            op_args=[TABLE_NAME, MODEL_INPUT_SCHEMA],
-            dag = Lead_scoring_inference_dag)
+    task_id='checking_model_inputs_schema',
+    python_callable=model_input_schema_check,
+    op_args=[TABLE_NAME, MODEL_INPUT_SCHEMA],
+    dag=Lead_scoring_inference_dag)
 
 
 ###############################################################################
 # Create a task for encode_data_task() function with task_id 'encoding_categorical_variables'
 # ##############################################################################
 encoding_categorical_variables = PythonOperator(
-        task_id = 'encoding_categorical_variables',
-        python_callable = encode_features,
-        dag = Lead_scoring_inference_dag)
+    task_id='encoding_categorical_variables',
+    python_callable=encode_features,
+    dag=Lead_scoring_inference_dag)
 
 # ###############################################################################
 # # Create a task for input_features_check() function with task_id 'checking_input_features'
@@ -110,20 +110,20 @@ encoding_categorical_variables = PythonOperator(
 # Create a task for load_model() function with task_id 'generating_models_prediction'
 # ##############################################################################
 generating_models_prediction = PythonOperator(
-        task_id = 'generating_models_prediction',
-        python_callable = get_models_prediction,
-        dag = Lead_scoring_inference_dag)
+    task_id='generating_models_prediction',
+    python_callable=get_models_prediction,
+    dag=Lead_scoring_inference_dag)
 
 
 ###############################################################################
 # Create a task for prediction_col_check() function with task_id 'checking_model_prediction_ratio'
 # ##############################################################################
 checking_model_prediction_ratio = PythonOperator(
-        task_id = 'checking_model_prediction_ratio',
-        python_callable = prediction_ratio_check,
-        dag = Lead_scoring_inference_dag)
+    task_id='checking_model_prediction_ratio',
+    python_callable=prediction_ratio_check,
+    dag=Lead_scoring_inference_dag)
 
 ###############################################################################
 # Define relation between tasks
 # ##############################################################################
-start_mlflow >> checking_raw_data_schema >> loading_data >> mapping_city_tier >> mapping_categorical_vars >> mapping_interactions >> checking_model_inputs_schema  >> encoding_categorical_variables >> generating_models_prediction >> checking_model_prediction_ratio
+start_mlflow >> checking_raw_data_schema >> loading_data >> mapping_city_tier >> mapping_categorical_vars >> mapping_interactions >> checking_model_inputs_schema >> encoding_categorical_variables >> generating_models_prediction >> checking_model_prediction_ratio
